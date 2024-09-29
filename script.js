7af0766ad63755740d4c3f32f63a68aabceb4398
Aden Neal <adenocbenji@gmail.com>
2024-09-28 19:39:32 -0500

    added pics

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['HTML','CSS', 'JavaScript', 'Node.js', 'Express', ],
+    strings: ['HTML','CSS', 'JavaScript', 'Node.js', 'Express', 'React'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
